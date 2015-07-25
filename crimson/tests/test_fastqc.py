f634f13a811a462e9b9b655eb81ef16f403925c7
bow <bow@bow.web.id>
2015-07-25 23:37:59 +0200

    Add fail test for fastqc

--- crimson/tests/test_fastqc.py
+++ crimson/tests/test_fastqc.py
@@ -19,6 +19,18 @@ from .utils import get_test_file, getattr_nested
 
 
 @pytest.fixture(scope="module")
+def fastqc_fail():
+    runner = CliRunner()
+    in_file = get_test_file("fastqc_nope.txt")
+    result = runner.invoke(cli, ["fastqc", in_file, "-"])
+    return result
+
+
+def test_fastqc_fail(fastqc_fail):
+    assert fastqc_fail.exit_code == -1
+
+
+@pytest.fixture(scope="module")
 def fastqc_v0101_01():
     runner = CliRunner()
     in_file = get_test_file("fastqc_v0101_01.txt")
