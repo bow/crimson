cd9c7deed9bc464d091358e65bb409bb2613bf29
bow <bow@bow.web.id>
2016-03-15 09:08:40 +0100

    Add initial STAR log parser

--- crimson/main.py
+++ crimson/main.py
@@ -15,6 +15,7 @@ from . import __version__
 from . import fastqc as m_fastqc
 from . import flagstat as m_flagstat
 from . import picard as m_picard
+from . import star as m_star
 from . import star_fusion as m_star_fusion
 from .utils import write_output
 
@@ -78,6 +79,20 @@ def picard(ctx, input, output):
 
     """
     payload = m_picard.parse(input)
+    write_output(payload, output, **ctx.parent.params)
+
+
+@cli.command()
+@click.argument("input", type=click.File("r"))
+@click.argument("output", type=click.File("w"), default="-")
+@click.pass_context
+def star(ctx, input, output):
+    """Converts STAR Log.final.out file.
+
+    Use "-" for stdin and/or stdout.
+
+    """
+    payload = m_star.parse(input)
     write_output(payload, output, **ctx.parent.params)
 
 
---
+++ crimson/star.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+    crimson.star
+    ~~~~~~~~~~~~
+
+    STAR Log.final.out file.
+
+    :copyright: (c) 2016 Wibowo Arindrarto <bow@bow.web.id>
+    :license: BSD
+
+"""
+import click
+
+from .utils import convert, get_handle
+
+
+__all__ = ["parse"]
+
+
+def _pct_convert(raw_str):
+    """Converts the given number ending with '%' to a number."""
+    if raw_str.endswith("%"):
+        return convert(raw_str[:-1])
+    return convert(raw_str)
+
+
+# Mapping of STAR attribute names to python dictionary attribute names
+# and the function used to parse it.
+_PARSE_MAP = {
+    "Started job on":
+        ("timeJobStart", str),
+    "Started mapping on":
+        ("timeMappingStart", str),
+    "Finished on":
+        ("timeEnd", str),
+    "Mapping speed, Million of reads per hour":
+        ("mappingSpeed", convert),
+    "Number of input reads":
+        ("nInput", convert),
+    "Average input read length":
+        ("avgInputLength", convert),
+    "Uniquely mapped reads number":
+        ("nUniquelyMapped", convert),
+    "Uniquely mapped reads %":
+        ("pctUniquelyMapped", _pct_convert),
+    "Average mapped length":
+        ("avgMappedLength", convert),
+    "Number of splices: Total":
+        ("nSplicesTotal", convert),
+    "Number of splices: Annotated (sjdb)":
+        ("nSplicesAnnotated", convert),
+    "Number of splices: GT/AG":
+        ("nSplicesGTAG", convert),
+    "Number of splices: GC/AG":
+        ("nSplicesGCAG", convert),
+    "Number of splices: AT/AC":
+        ("nSplicesATAC", convert),
+    "Number of splices: Non-canonical":
+        ("nSplicesNonCanonical", convert),
+    "Mismatch rate per base, %":
+        ("rateMismatchPerBase", _pct_convert),
+    "Deletion rate per base":
+        ("rateDeletionPerBase", _pct_convert),
+    "Deletion average length":
+        ("avgDeletionLength", convert),
+    "Insertion rate per base":
+        ("rateInsertionPerBase", _pct_convert),
+    "Insertion average length":
+        ("avgInsertionLength", convert),
+    "Number of reads mapped to multiple loci":
+        ("nMappedMultipleLoci", convert),
+    "% of reads mapped to multiple loci":
+        ("pctMappedMultipleLoci", _pct_convert),
+    "Number of reads mapped to too many loci":
+        ("nMappedTooManyLoci", convert),
+    "% of reads mapped to too many loci":
+        ("pctMappedTooManyLoci", _pct_convert),
+    "% of reads unmapped: too many mismatches":
+        ("pctUnmappedForTooManyMismatches", _pct_convert),
+    "% of reads unmapped: too short":
+        ("pctUnmappedForTooShort", _pct_convert),
+    "% of reads unmapped: other":
+        ("pctUnmappedForOther", _pct_convert),
+}
+
+
+def parse(in_data):
+    """Parses the log of a STAR run.
+
+    :param in_data: Input STAR-Fusion contents.
+    :type in_data: str or file handle
+    :returns: Parsed values.
+    :rtype: dict
+
+    """
+    payload = {}
+    with get_handle(in_data) as src:
+        for line in src:
+            # pass empty lines
+            if line and not line.strip():
+                continue
+            line = line.strip()
+            if '|' in line:
+                ori_key, val = [x.strip() for x in line.split('|', 1)]
+                if ori_key in _PARSE_MAP:
+                    key, func = _PARSE_MAP[ori_key]
+                    val = func(val)
+                if key in payload:
+                    msg = "Unexpected duplicate key entry: {0} ({1})."
+                    raise click.BadParameter(msg.format(key, ori_key))
+                payload[key] = val
+
+    return payload
