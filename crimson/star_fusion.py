ae6fcdb2428878a0a6a3cf7daa3aefc3c2f7c1c9
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2020-01-23 16:35:32 +0100

    Rewrite of star-fusion to work with v1.6.0

--- crimson/star_fusion.py
+++ crimson/star_fusion.py
@@ -9,7 +9,7 @@
 # (c) 2015-2020 Wibowo Arindrarto <bow@bow.web.id>
 
 from os import PathLike
-from typing import List, TextIO, Union
+from typing import Any, Dict, List, TextIO, Tuple, Union
 
 import click
 
@@ -52,22 +52,41 @@ SUPPORTED = {
 # Special columns, currently shared by all output formats
 # These have to be parsed in special way
 SPECIAL = ["LeftGene", "LeftBreakpoint", "RightGene", "RightBreakpoint"]
+
 # Mapping of supported columns to output format
 #
 # Column name in file -> field name in json
 #
 # Note: the JunctionReads and SpanningFrags columns are present twice in
 # the output files, with different meanings and content
+# SPECIAL columns are excluded
 COL_MAPPING = {
     'v0.6.0': {
         'fusion_name': 'fusionName',
         'JunctionReads': 'nJunctionReads',
         'SpanningFrags': 'nSpanningFrags',
-        'Splice_type': 'spliceType',
-        'LeftGene': 'left',
-        'LeftBreakpoint': 'LeftBreakpoint',
-        'RightGene': 'right',
-        'RightBreakpoint': 'RightBreakpoint',
+        'Splice_type': 'spliceType'
+    },
+    'v0.6.0_abr': {
+        'fusion_name': 'fusionName',
+        'JunctionReads': 'nJunctionReads',
+        'SpanningFrags': 'nSpanningFrags',
+        'Splice_type': 'spliceType'
+    },
+    'v1.6.0': {
+        'FusionName': 'fusionName',
+        'JunctionReadCount': 'nJunctionReads',
+        'SpanningFragCount': 'nSpanningFrags',
+        'SpliceType': 'spliceType',
+        'JunctionReads': 'junctionReads',
+        'SpanningFrags': 'spanningFrags',
+        'LargeAnchorSupport': 'largeAnchorSupport',
+        'FFPM': 'FFPM',
+        'LeftBreakDinuc': 'leftBreakDinuc',
+        'LeftBreakEntropy': 'leftBreakEntropy',
+        'RightBreakDinuc': 'rightBreakDinuc',
+        'RightBreakEntropy': 'rightBreakEntropy',
+        'annots': 'annots'
     }
 }
 
@@ -80,7 +99,10 @@ _DELIM = {
 }
 
 
-def parse_lr_entry(lr_gene: str, lr_brkpoint: str) -> dict:
+def parse_lr_entry(
+    lr_gene: str,
+    lr_brkpoint: str
+) -> Dict[str, Union[str, int]]:
     """Parse the gene and breakpoint entry.
 
     :param lr_gene: Column value for 'LeftGene' or 'RightGene'.
@@ -99,11 +121,56 @@ def parse_lr_entry(lr_gene: str, lr_brkpoint: str) -> dict:
     }
 
 
+def parse_read_columns(
+    colnames: List[str],
+    values: List[str]
+) -> Tuple[Dict[str, str], Dict[str, List[str]]]:
+    """Parse the read columns out and return them seperately
+
+    The JunctionReads and SpanningFrags columns can contain the actual reads,
+    or just the counts of the reads. This makes parsing them correctly quite
+    convoluted.
+
+    Assumption: The last occurrence of the JunctionReads and SpanningFrags
+    columns in the file contain the actual reads, while the first one contains
+    the counts.
+    If there is only one (as is the case for v1.6.0), it contains the reads
+
+    :param values: List of values from the file
+    :para colnames: List of column names from the file
+    """
+    entries = dict()
+    reads = dict()
+
+    # Keep the column names and values together
+    together = [(name, val) for name, val in zip(colnames, values)]
+
+    # Extract the JunctionReads
+    rev = together[::-1]
+    for name, val in rev:
+        if name == 'JunctionReads':
+            together.remove((name, val))
+            break
+    reads[name] = val.split(',')
+
+    # Extract the SpanningFrags
+    for name, val in rev:
+        if name == 'SpanningFrags':
+            together.remove((name, val))
+            break
+    reads[name] = val.split(',')
+
+    # The other columns are regular entries
+    entries = {k: v for k, v in together}
+
+    return entries, reads
+
+
 def parse_raw_line(
     raw_line: str,
     version: str,
     is_abridged: bool = True,
-) -> dict:
+) -> Dict[str, Any]:
     """Parse a single line into a dictionary.
 
     :param raw_line: STAR-Fusion result line.
@@ -118,44 +185,41 @@ def parse_raw_line(
         msg = "Line values {0} does not match column names {1}."
         raise click.BadParameter(msg.format(values, colnames))
 
-    entries, reads = {}, {}
     if is_abridged:
         entries = {k: v for k, v in zip(colnames, values)}
+        reads = dict()  # type: Dict[str, List[str]]
     else:
-        entry_colnames, read_colnames = colnames[:-2], colnames[-2:]
-        entry_values, read_values = values[:-2], values[-2:]
-        entries = {k: v for k, v in zip(entry_colnames, entry_values)}
-        reads = {k: v.split(",") for k, v in zip(read_colnames, read_values)}
+        # If the format is not abridged, the JunctionReads and SpanningFrags
+        # columns are duplicated
+        entries, reads = parse_read_columns(colnames, values)
 
-    ret = {
-        "fusionName": entries["fusion_name"],
-        "nJunctionReads": int(entries["JunctionReads"]),
-        "nSpanningFrags": int(entries["SpanningFrags"]),
-        "spliceType": entries["Splice_type"],
-        "left": parse_lr_entry(entries["LeftGene"],
-                               entries["LeftBreakpoint"]),
-        "right": parse_lr_entry(entries["RightGene"],
-                                entries["RightBreakpoint"]),
-    }
     # Create the output dictionary based on the detected star-fusion version
-    ret2 = dict()
+    ret = dict()  # type: Dict[str, Any]
     for colname in entries:
         if colname in SPECIAL:
             continue
         field_name = COL_MAPPING[version][colname]
-        ret2[field_name] = entries[colname]
+        ret[field_name] = entries[colname]
 
     # Cast the apropriate entries to int
-    ret2['nJunctionReads'] = int(ret2['nJunctionReads'])
-    ret2['nSpanningFrags'] = int(ret2['nSpanningFrags'])
+    # These values should always exist
+    for int_field in ['nJunctionReads', 'nSpanningFrags']:
+        ret[int_field] = int(ret[int_field])
+
+    # Cast the apropriate entries to float
+    # These values can be missing
+    for float_field in ['FFPM', 'leftBreakEntropy', 'rightBreakEntropy']:
+        try:
+            ret[float_field] = float(ret[float_field])
+        except KeyError:
+            continue
 
     # Handle the SPECIAL columns
-    ret2["left"] = parse_lr_entry(entries["LeftGene"],
-                                  entries["LeftBreakpoint"])
-    ret2["right"] = parse_lr_entry(entries["RightGene"],
-                                   entries["RightBreakpoint"])
+    ret["left"] = parse_lr_entry(entries["LeftGene"],
+                                 entries["LeftBreakpoint"])
+    ret["right"] = parse_lr_entry(entries["RightGene"],
+                                  entries["RightBreakpoint"])
 
-    ret = ret2
     if reads:
         ret["reads"] = {
             "junctionReads": reads["JunctionReads"],
@@ -189,8 +253,6 @@ def parse(in_data: Union[str, PathLike, TextIO]) -> List[dict]:
             raise click.BadParameter(msg.format(first_line))
         # Parse column names, after removing the '#' character
         colnames = first_line[1:].split("\t")
-        # print(first_line)
-        # print(colnames)
         version = detect_format(colnames)
         is_abridged = version == 'v0.6.0_abr'
         for line in (x.strip() for x in src):
