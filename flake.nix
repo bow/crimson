38df2f545f65d7d89fb4497e1baab9b45bb09051
Wibowo Arindrarto <contact@arindrarto.dev>
2025-06-11 21:37:20 +0200

    chore: pin nixpkgs to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix flake for Crimson";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
